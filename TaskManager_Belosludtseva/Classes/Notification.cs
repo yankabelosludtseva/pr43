afbb9321245bff3d7ffd915ee8c39903192ac6e9
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 14:47:38 +0500

    Разработанный классы общего-пользования. Белослудцева

---
+++ TaskManager_Belosludtseva/Classes/Notification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Belosludtseva.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangingEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TaskManager_Belosludtseva/Classes/RealyCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TaskManager_Belosludtseva.Classes
+{
+    public class RealyCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public RealyCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter) =>
+            this.execute(parameter);
+    }
+}
