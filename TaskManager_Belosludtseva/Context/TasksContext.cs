c274c3a6f1a77444cc573ae634b3b66715fef67e
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 15:00:09 +0500

    Разработанный контекст данных для модели задач. Белослудцева

---
+++ TaskManager_Belosludtseva/Context/TasksContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Belosludtseva.Classes.Database;
+using TaskManager_Belosludtseva.Models;
+
+namespace TaskManager_Belosludtseva.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks {  get; set; }
+
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+        }
+    }
+}
---
+++ TaskManager_Belosludtseva/Models/Tasks.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using TaskManager_Belosludtseva.Classes;
+using TaskManager_Belosludtseva.ViewModels;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager_Belosludtseva.Models
+{
+    public class Tasks : Notification
+    {
+        public int Id { get; set; }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+
+                Match match = Regex.Match(value, "^(.{1,50})$");
+                if (!match.Success)
+                    MessageBox.Show("Наименование не должно быть пустым, и не более 50 символов.",
+                        "Не корректный ввод значения.");
+                else
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string priority;
+        public string Priority
+        {
+            get { return priority; }
+            set
+            {
+                Match match = Regex.Match(value, "^(.{1,30})$");
+                if (!match.Success)
+                    MessageBox.Show("Приоритет не должно быть пустым, и не более 30 символов.",
+                        "Не корректный ввод значения.");
+                else
+                {
+                    priority = value;
+                    OnPropertyChanged("Priority");
+                }
+            }
+        }
+
+        private DateTime dateExecute;
+        public DateTime DateExecute
+        {
+            get { return dateExecute; }
+            set
+            {
+
+                if (value.Date < DateTime.Now.Date)
+                    MessageBox.Show("Дата выполнения не может быть меньше текущей.",
+                        "Не корректный ввод значения.");
+                else
+                {
+                    dateExecute = value;
+                    OnPropertyChanged("DateExecute");
+                }
+            }
+        }
+
+        private string comment;
+        public string Comment
+        {
+            get { return comment; }
+            set
+            {
+                Match match = Regex.Match(value, "^(.{1,1000})$");
+                if (!match.Success)
+                    MessageBox.Show("Комментарий не должен быть пустым, и не более 1000 символов.",
+                        "Не корректный ввод значения.");
+                else
+                {
+                    comment = value;
+                    OnPropertyChanged("Comment");
+                }
+            }
+        }
+
+        public bool done;
+        public bool Done
+        {
+            get { return done; }
+            set
+            {
+                done = value;
+                OnPropertyChanged("Done");
+                OnPropertyChanged("IsDoneText");
+            }
+        }
+
+        [Schema.NotMapped]
+        private bool isEnable;
+
+        [Schema.NotMapped]
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set
+            {
+                isEnable = value;
+                OnPropertyChanged("IsEnable");
+                OnPropertyChanged("IsEnableText");
+            }
+        }
+
+        [Schema.NotMapped]
+        public string IsEnableText
+        {
+            get
+            {
+                if (IsEnable) return "Сохранить";
+                else return "Изменить";
+            }
+        }
+
+        [Schema.NotMapped]
+        public string IsDoneText
+        {
+            get
+            {
+                if (Done) return "Не выполнено";
+                else return "Выполнено";
+            }
+        }
+
+        [Schema.NotMapped]
+        public RealyCommand OnEdit
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    IsEnable = !IsEnable;
+
+                    if (!IsEnable)
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+                });
+            }
+        }
+
+        [Schema.NotMapped]
+        public RealyCommand OnDelete
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    if (MessageBox.Show("Вы уверены что хотите удалить задачу?",
+                        "Предупреждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.Tasks.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+                    }
+                });
+            }
+        }
+
+        [Schema.NotMapped]
+        public RealyCommand OnDone
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    Done = !Done;
+                });
+            }
+        }
+    }
+}
