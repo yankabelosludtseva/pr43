6fc8220fa761b510b7c0450f58c2e7884eeeeef1
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 15:06:36 +0500

    Разработанная модель представления главного окна. Белослудцева

---
+++ TaskManager_Belosludtseva/ViewModels/VM_Pages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Belosludtseva.Classes;
+
+namespace TaskManager_Belosludtseva.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+
+        public VM_Pages() 
+        {
+            MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+        }
+
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
