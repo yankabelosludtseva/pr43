f688df3f18fa9803121d6ef07b50c16003a60082
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 14:40:47 +0500

    Созданный класс Config. Белослудцева

---
+++ TaskManager_Belosludtseva/Classes/Database/Config.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Belosludtseva.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=192.168.0.111; uid=root; pwd=; database=TaskManager;";
+
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
