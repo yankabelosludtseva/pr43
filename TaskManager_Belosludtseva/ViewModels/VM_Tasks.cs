1195f91718bf9501b92934555783fb27b54e0434
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 15:12:12 +0500

    Разработанная модель представления задач. Белослудцева

---
+++ TaskManager_Belosludtseva/ViewModels/VM_Tasks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Belosludtseva.Classes;
+using TaskManager_Belosludtseva.Context;
+using TaskManager_Belosludtseva.Models;
+
+namespace TaskManager_Belosludtseva.ViewModels
+{
+    public class VM_Tasks : Notification
+    {
+        public TasksContext tasksContext = new TasksContext();
+
+        public ObservableCollection<Tasks> Tasks { get; set; }
+
+        public VM_Tasks() =>
+            Tasks = new ObservableCollection<Tasks>(tasksContext.Tasks.OrderBy(x => x.Done));
+
+        public RealyCommand OnAddTask
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    Tasks NewTask = new Tasks()
+                    {
+                        DateExecute = DateTime.Now
+                    };
+                    Tasks.Add(NewTask);
+                    tasksContext.Tasks.Add(NewTask);
+                    tasksContext.SaveChanges();
+                });
+            }
+        }
+    }
+}
